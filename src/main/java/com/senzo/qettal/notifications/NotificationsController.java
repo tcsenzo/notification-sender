5368a7071e1068ca45ad897f7929cc065e6754be
leonardowolter <leocwolter@gmail.com>
2016-09-07 17:06:58 -0300

    Extracts urls to application.properties

--- src/main/java/com/senzo/qettal/notifications/NotificationsController.java
+++ src/main/java/com/senzo/qettal/notifications/NotificationsController.java
@@ -3,6 +3,7 @@ package com.senzo.qettal.notifications;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,11 +15,13 @@ public class NotificationsController {
 
 	@Autowired
 	private List<NotificationSender> senders;
+	@Value("${url.theaterEvents}")
+	private String eventsUrl;
 	
 	@RequestMapping("/send")
 	public EventListDTO send(){
 		RestTemplate restTemplate = new RestTemplate();
-		EventListDTO events = restTemplate.getForObject("http://localhost:8080/events", EventListDTO.class);
+		EventListDTO events = restTemplate.getForObject(eventsUrl, EventListDTO.class);
 		for (NotificationSender sender : senders) {
 			sender.send(events);
 		}
